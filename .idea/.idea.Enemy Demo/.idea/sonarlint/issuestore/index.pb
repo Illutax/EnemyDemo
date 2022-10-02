e157ba82fd526407bb7facfb95ee3556fb22e33f
Valerij Dobler <vd@wps.de>
2022-10-02 12:20:48 +0200

    Adds pickup item

--- .idea/.idea.Enemy Demo/.idea/sonarlint/issuestore/index.pb
+++ .idea/.idea.Enemy Demo/.idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
 N
 Assets/Scripts/EnemySpawner.cs,9\3\9362b09db8ca8cfffec6c5934d5c64de96974d72
-H
-Assets/Scripts/Player.cs,3\2\32367782be3d16f3ab4628d48a5765e7c8773b11
---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PickupItem : MonoBehaviour
+    {
+        public int xpAmount = 25;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                print("Picked up pickup");
+                player.GainXP(xpAmount);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
